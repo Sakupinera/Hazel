10f4e18231547a6253c3a4bd1cd8922ad5fa7bf3
Sakupinera <Sakupinera@gmail.com>
2024-03-20 09:46:18 +0800

    [Hazel] Moved 'Application' class code to Sandbox and Added Timestep.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,17 +7,13 @@
 #include "Events/Event.h"
 #include "Events/ApplicationEvent.h"
 
+#include "Hazel/Core/Timestep.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
-
-#include "Hazel/Renderer/Shader.h"
-#include "Hazel/Renderer/Buffer.h"
-#include "Renderer/VertexArray.h"
-
-#include "Hazel/Renderer/OrthographicCamera.h"
 
 namespace Hazel
 {
-	class HAZEL_API Application
+	class Application
 	{
 	public:
 		Application();
@@ -40,14 +36,7 @@ namespace Hazel
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
-
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
-
-		std::shared_ptr<Shader> m_BlueShader;
-		std::shared_ptr<VertexArray> m_SquareVA;
-
-		OrthographicCamera m_Camera;
+		float m_LastFrameTime = 0.0f;
 	private:
 		static Application* s_Instance;
 	};
