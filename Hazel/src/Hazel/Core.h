1f0857c04b69af4d6e7a20bfe73e6b33ed0198b3
Sakupinera <Sakupinera@gmail.com>
2024-03-27 09:03:19 +0800

    [Hazel] Refs, Scopes and Smart Pointers.

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #define HZ_DYNAMIC_LINK 0
 
 #ifdef HZ_PLATFORM_WINDOWS
@@ -31,3 +33,13 @@
 #define BIT(x) (1 << x)
 
 #define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Hazel {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
