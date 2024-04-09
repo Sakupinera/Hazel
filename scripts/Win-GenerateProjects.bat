e0af967893deecea94645a0597c67b3d8f0b69cf
Sakupinera <Sakupinera@gmail.com>
2024-04-09 20:31:26 +0800

    [Hazel] Maintenance.

---
+++ scripts/Win-GenerateProjects.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Please choose from the following options:
+echo [1] Premake
+echo [2] CMake
+set /p choice=Please enter your choice (1 or 2):
+if "%choice%"=="1" (
+    echo You chose 1, Premake related code will be executed...
+    pushd ..\
+    call vendor-ex\bin\premake\premake5.exe vs2022
+    popd
+    PAUSE
+) else if "%choice%"=="2" (
+    echo You chose 2, CMake related code will be executed...
+    cd ..
+    mkdir build
+    cd build
+    cmake ..
+    :: cmake --build . --config Debug
+    PAUSE
+) else (
+    echo Invalid input! Please rerun the script and enter 1 or 2.
+    PAUSE
+)
