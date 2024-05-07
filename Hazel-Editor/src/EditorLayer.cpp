f3a797696882635ee1008635cf2cbd29d6d0d617
Sakupinera <sakupinera@gmail.com>
2024-05-07 11:53:41 +0800

    [Hazel] Camera Systems.

--- Hazel-Editor/src/EditorLayer.cpp
+++ Hazel-Editor/src/EditorLayer.cpp
@@ -30,6 +30,13 @@ namespace Hazel
 
 		m_SquareEntity = square;
 
+		m_CameraEntity = m_ActiveScene->CreateEntity("Camera Entity");
+		m_CameraEntity.AddComponent<CameraComponent>(glm::ortho(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f));
+
+		m_SecondCamera = m_ActiveScene->CreateEntity("Clip-Space Entity");
+		auto& cc = m_SecondCamera.AddComponent<CameraComponent>(glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f));
+		cc.Primary = false;
+
 		m_CameraController.SetZoomLevel(5.0f);
 	}
 
@@ -59,6 +66,7 @@ namespace Hazel
 		Renderer2D::ResetStats();
 		{
 			HZ_PROFILE_SCOPE("Renderer Prep");
+
 			m_Framebuffer->Bind();
 			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 			RenderCommand::Clear();
@@ -66,12 +74,9 @@ namespace Hazel
 
 		{
 			HZ_PROFILE_SCOPE("Renderer Draw");
-			Renderer2D::BeginScene(m_CameraController.GetCamera());
 
 			// Update scene
 			m_ActiveScene->OnUpdate(ts);
-
-			Renderer2D::EndScene();
 		}
 
 		m_Framebuffer->Unbind();
@@ -158,6 +163,15 @@ namespace Hazel
 				auto& squareColor = m_SquareEntity.GetComponent<SpriteRendererComponent>().Color;
 				ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
 				ImGui::Separator();
+			}
+
+			ImGui::DragFloat3("Camera Transform",
+				glm::value_ptr(m_CameraEntity.GetComponent<TransformComponent>().Transform[3]));
+
+			if (ImGui::Checkbox("Camera A", &m_PrimaryCamera))
+			{
+				m_CameraEntity.GetComponent<CameraComponent>().Primary = m_PrimaryCamera;
+				m_SecondCamera.GetComponent<CameraComponent>().Primary = !m_PrimaryCamera;
 			}
 
 			ImGui::End();
