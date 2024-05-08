24b9168e1f0eb4f4624634ffc2bf6f327c544ca2
Sakupinera <sakupinera@gmail.com>
2024-05-08 08:52:23 +0800

    [Hazel] Native Scripting.

--- Hazel/src/Hazel/Scene/Components.h
+++ Hazel/src/Hazel/Scene/Components.h
@@ -3,6 +3,7 @@
 #include <glm/glm.hpp>
 
 #include "Hazel/Scene/SceneCamera.h"
+#include "Hazel/Scene/ScriptableEntity.h"
 
 namespace Hazel {
 
@@ -47,6 +48,35 @@ namespace Hazel {
 
 		CameraComponent() = default;
 		CameraComponent(const CameraComponent&) = default;
+	};
+
+	struct NativeScriptComponent
+	{
+		ScriptableEntity* Instance = nullptr;
+
+		//std::function<void()> InstantiateFunction;
+		//std::function<void()> DestroyInstanceFunction;
+
+		//std::function<void(ScriptableEntity*)> OnCreateFunction;
+		//std::function<void(ScriptableEntity*)> OnDestroyFunction;
+		//std::function<void(ScriptableEntity*, Timestep)> OnUpdateFunction;
+
+		ScriptableEntity* (*InstantiateScript)();
+		void (*DestroyScript)(NativeScriptComponent*);
+
+		template<typename T>
+		void Bind()
+		{
+			//InstantiateFunction = [&]() { Instance = new T(); };
+			//DestroyInstanceFunction = [&]() { delete (T*)Instance; Instance = nullptr; };
+
+			//OnCreateFunction = [](ScriptableEntity* instance) { ((T*)instance)->OnCreate(); };
+			//OnDestroyFunction = [](ScriptableEntity* instance) { ((T*)instance)->OnDestroy(); };
+			//OnUpdateFunction = [](ScriptableEntity* instance, Timestep ts) { ((T*)instance)->OnUpdate(ts); };
+
+			InstantiateScript = []() { return static_cast<ScriptableEntity*>(new T()); };
+			DestroyScript = [](NativeScriptComponent* nsc) { delete nsc->Instance; nsc->Instance = nullptr; };
+		}
 	};
 
 }
