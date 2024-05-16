bac3eef260dbab0ce5a16880360b10c1e8b1a0a9
Sakupinera <sakupinera@gmail.com>
2024-05-16 14:23:37 +0800

    [Hazel] Multiple Render Targets and Framebuffer Refactor.

--- Hazel/src/Hazel/Renderer/Framebuffer.cpp
+++ Hazel/src/Hazel/Renderer/Framebuffer.cpp
@@ -11,8 +11,8 @@ namespace Hazel {
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLFramebuffer>(spec);
+			case RendererAPI::API::None:    HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:  return CreateRef<OpenGLFramebuffer>(spec);
 		}
 
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
